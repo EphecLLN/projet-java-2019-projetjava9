4ae1a2b16756ed38623b414f06b772b6468f5ea3
Bouquoyoue Bilel <HE201666@students.ephec.be>
2019-12-15 17:59:31 +0100

    Remise en forme Bonus
    Les murs actif et vitesse + menu

--- Snake_Projet/src/snakeProj/snakePro_test.java
+++ Snake_Projet/src/snakeProj/snakePro_test.java
@@ -17,12 +17,17 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import snakeProj.creerOeuf;
+import snakeProj.snakePro_test.PartieSerpent;
 
 
  
 public class snakePro_test extends JPanel{
 	
-    private static int WIDTH = 25;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static int WIDTH = 25;
     public static Deque<PartieSerpent> snake = new ArrayDeque<>();
     //lie la variable oeuf de la classe creerOeuf
     private Point oeuf = snakeProj.creerOeuf.oeuf;
@@ -41,8 +46,10 @@ public class snakePro_test extends JPanel{
     public JPanel panel;
     public int compteurOff = 1;
     private Point bonus = new Point(7, 7);
-    public int compteurOeuf = 0;
+    public int BoostVitesse = 0;
+    public int BonusOeuf = 0;
     private int offsetIncrementValueBonus = 10;
+    public int comparateur = 1;
    
 
     
@@ -51,7 +58,7 @@ public class snakePro_test extends JPanel{
     	egg.creerOeuf();
     	
     	//Fenêtre
-        JFrame frame = new JFrame("Snake");
+        JFrame frame = new JFrame("Snake Un Jeu");
         JPanel panel = new snakePro_test();
         
         frame.addKeyListener(new KeyListener() {
@@ -87,7 +94,7 @@ public class snakePro_test extends JPanel{
         //Bouton activer les murs ou désactiver
         JMenuItem NewGame = new JMenuItem("Nouvelle Partie");
         JMenuItem ActiverMurs = new JMenuItem("Activer les murs");
-        JMenuItem DesactiverMurs = new JMenuItem("Désactiver les murs");
+        JMenuItem DesactiverMurs = new JMenuItem("D�sactiver les murs");
         Options.add(ActiverMurs);
         Options.add(DesactiverMurs);
         Commencer.add(NewGame);
@@ -139,92 +146,110 @@ public class snakePro_test extends JPanel{
    
     @Override
     protected void paintComponent(Graphics objet) {
-    	if(debutPartie == 1) {     			
-        super.paintComponent(objet);
-        offset += offsetIncrementValue;
-        PartieSerpent head = null;
-        if(offset > WIDTH) {
-            offset = 0;
-            try {
-                head = (PartieSerpent) snake.getFirst().clone();
-                head.move();
-                head.direction = newDirection;
-                snake.addFirst(head);
-                if(head.x == oeuf.x && head.y == oeuf.y) {
-                    Croissance = true;
-                    eggEaten = true;
-                    creerOeuf egg = new creerOeuf();
-                    egg.creerOeuf();
-                    compteurOeuf++;
-                    offsetIncrementValue = 5;
-                    if(compteurOeuf == 7) {
-                    	creerBonus();
-                    	compteurOeuf = 0;
-                    }
-
-                    score++;  
-                }
-                if(!Croissance) {
-                    snake.pollLast();  
-                }
-                else {
-                    Croissance = false;
-                }
-                if(head.x == bonus.x && head.y == bonus.y) {
-                	bonus = new Point(0, 0);
-                	offsetIncrementValue = offsetIncrementValueBonus;
-                }
-                
-            }
-            catch (CloneNotSupportedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }    	
-        objet.setColor(Color.YELLOW);
-        objet.fillOval(oeuf.x*WIDTH + WIDTH/4, oeuf.y*WIDTH + WIDTH/4, WIDTH/2, WIDTH/2);
-        
-        /*objet.setColor(Color.RED);
-        objet.fillOval(oeuf.x*WIDTH + WIDTH/4, oeuf.y*WIDTH + WIDTH/4, WIDTH/2, WIDTH/2);*/
-        
-        if(score < 10) {
-            setBackground(Color.DARK_GRAY);
-            }  
-        if(score>10 && snake.size()<20) {
-        	setBackground(Color.gray);
-        }
-        else if(score>20 && snake.size()<30) {
-        	setBackground(Color.CYAN);
-        }
-        else if(score>30 && snake.size() < 40) {
-        	setBackground(Color.BLACK);
-        }
-        objet.setColor(Color.GREEN);
-        for(PartieSerpent p : snake) {
-            if(offset == 0) {
-                if(p != head) {
-                    if(p.x == head.x && p.y == head.y) {
-                        gameLost = 1;
-                        debutPartie = 0;
-                    }
-                }
-            }
-            if(p.direction == 37 || p.direction == 39) {
-                objet.fillRect(p.x * WIDTH + ((p.direction == 37) ? -offset : offset), p.y*WIDTH, WIDTH, WIDTH);
-            } 
-            else {
-                objet.fillRect(p.x * WIDTH, p.y*WIDTH + ((p.direction == 38) ? -offset : offset), WIDTH, WIDTH);
-            }
-        }        
-        objet.setColor(Color.GREEN);
-        objet.setColor(Color.BLUE);
-        objet.drawString("Score : "+ score, 9, 45);
+    	//Malus Murs Actifs si 10 oeufs mang�s
+    	if(mursActifs == false) {
+	    	if(score % 10 == 1 && score != 1) {
+	        	mursActifs = true;
+	    	} 	
+	    }
+    	//Debut de Partie
+    	if(debutPartie == 1) {
+	        super.paintComponent(objet);
+	        offset += offsetIncrementValue;
+	        PartieSerpent head = null;
+	        if(offset > WIDTH) {
+	            offset = 0;
+	            try {
+	                head = (PartieSerpent) snake.getFirst().clone();
+	                head.move();
+	                head.direction = newDirection;
+	                snake.addFirst(head);
+	                if(head.x == oeuf.x && head.y == oeuf.y) {
+	                	mursActifs = false;
+	                    Croissance = true;
+	                    eggEaten = true;
+	                    creerOeuf egg = new creerOeuf();
+	                    egg.creerOeuf();
+	                    BoostVitesse++;
+	                    offsetIncrementValue = 5;
+	                    if(BoostVitesse == 7) {
+	                    	creerBonus();
+	                    	BoostVitesse = 0;
+	                    }
+	                    score++;  
+	                }
+	                if(!Croissance) {
+	                    snake.pollLast();  
+	                }
+	                else {
+	                    Croissance = false;
+	                }
+	                if(head.x == bonus.x && head.y == bonus.y) {
+	                	bonus = new Point(0, 0);
+	                	offsetIncrementValue = offsetIncrementValueBonus;
+	                }
+	                
+	            }
+	            catch (CloneNotSupportedException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            }
+	        } 
+	        if(score % 10 == 0.00 && score != 0) {
+	        	objet.setColor(Color.RED);
+	        }
+	        else if(score % 10 != 0.00 || score == 0) {
+	        objet.setColor(Color.YELLOW);
+	        }
+	        objet.fillOval(oeuf.x*WIDTH + WIDTH/4, oeuf.y*WIDTH + WIDTH/4, WIDTH/2, WIDTH/2);
+	        
+	        /*objet.setColor(Color.RED);
+	        objet.fillOval(oeuf.x*WIDTH + WIDTH/4, oeuf.y*WIDTH + WIDTH/4, WIDTH/2, WIDTH/2);*/
+	        
+	        if(score < 10) {
+	            setBackground(Color.DARK_GRAY);
+	            }  
+	        if(score>10 && snake.size()<20) {
+	        	setBackground(Color.gray);
+	        }
+	        else if(score>20 && snake.size()<30) {
+	        	setBackground(Color.CYAN);
+	        }
+	        else if(score>30 && snake.size() < 40) {
+	        	setBackground(Color.BLACK);
+	        }
+	        objet.setColor(Color.GREEN);
+	        for(PartieSerpent p : snake) {
+	            if(offset == 0) {
+	                if(p != head) {
+	                    if(p.x == head.x && p.y == head.y) {
+	                        gameLost = 1;
+	                        debutPartie = 0;
+	                    }
+	                }
+	            }
+	            if(p.direction == 37 || p.direction == 39) {
+	                objet.fillRect(p.x * WIDTH + ((p.direction == 37) ? -offset : offset), p.y*WIDTH, WIDTH, WIDTH);
+	            } 
+	            else {
+	                objet.fillRect(p.x * WIDTH, p.y*WIDTH + ((p.direction == 38) ? -offset : offset), WIDTH, WIDTH);
+	            }
+	        }        
+	        objet.setColor(Color.GREEN);
+	        objet.setColor(Color.BLUE);
+	        objet.drawString("Score : "+ score, 9, 45);
+	        
+	        if(score % 10 == 1 && score != 1) {
+	        	objet.setColor(Color.GREEN);
+	            objet.setColor(Color.BLUE);
+	            objet.drawString("Les murs sont actifs", 180, 300);	
+	        }
         }
     	else {
     		if(gameLost>0) {
                 objet.setColor(Color.RED);
-                objet.setFont(new Font("Arial", 55, 55));
-                objet.drawString("GAME OVER", 2125/2 - objet.getFontMetrics().stringWidth("Partie terminée")/2, 1925/2);
+                objet.setFont(new Font("Arial", 40, 40));
+                objet.drawString("GAME OVER", 110, 9*25);
                 if(gameLost++<2){
                     pseudoPlayer pseudo=new pseudoPlayer();
                     pseudo.pseudoPlayer();
@@ -232,6 +257,12 @@ public class snakePro_test extends JPanel{
                 }
                 return;
             }
+    		else {
+    			objet.setColor(Color.orange);
+        		objet.setFont(new Font("Arial", 40, 40));
+        		objet.drawString("Snake Un Jeu", 110, 5*25);
+    		}
+    		
     	}	
     }
     
--- Snake_Projet/src/snakeProj/song.java
+++ Snake_Projet/src/snakeProj/song.java
@@ -1,6 +1,6 @@
 package snakeProj;
 
-import sun.audio.*;
+//import sun.audio.*;
 
  
  
