1f9d7e86a3fcc7ebb85884423d1c0fa64ae1ac6a
jeanmtang <32909559+jeanmtang@users.noreply.github.com>
2019-12-15 19:59:22 +0100

    Add Javadoc

--- Snake_Projet/src/snakeProj/connectToGame.java
+++ Snake_Projet/src/snakeProj/connectToGame.java
@@ -3,45 +3,50 @@ package snakeProj;
 import java.awt.Point;
 
 import java.sql.*;
+import snakeProj.snakePro_test;
+import snakeProj.pseudoPlayer;
 
-
+/**
+ * The class connectToGame sends the data of the game in the database
+ */
 public class connectToGame {
-private Connection con;
-private Statement st;
-private ResultSet rs;
-private int score= snakeProj.snakePro_test.score;
-private String idPlayer= snakeProj.pseudoPlayer.idPlayer;
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+	private int score = snakeProj.snakePro_test.score;
+	private String idPlayer= snakeProj.pseudoPlayer.idPlayer;
 
+	/**
+	 * Method that links the game to the database
+	 */
 	public connectToGame() {
 	    try {
-	      Class.forName("org.postgresql.Driver");
-	         
-	      String url = "jdbc:postgresql://localhost:5432/srvGame";
-	      String user = "postgres";
-	      String passwd = "moimoimoi02";
-	         
-	      con = DriverManager.getConnection(url, user, passwd);
-	         
-	      //Création d'un objet Statement
-	      st  = con.createStatement();
-	      //L'objet dataSend contient le résultat de la requête SQL
+	    	// Links the class to the postgresql database
+	    	Class.forName("org.postgresql.Driver");
 
+			String url = "jdbc:postgresql://localhost:5432/srvGame";
+			String user = "postgres";
+			String passwd = "moimoimoi02";
 
-	         
-	    } catch (Exception e) {
-	     
-	    }   
-	  }
-	  public void dataSend() {
-		    try {
-		        System.out.println("idPlayer dans l'envoie "+idPlayer);
-		        System.out.println("score dans l'envoie "+score);
-		    	String query ="insert into savescore(id_player,score_player)Values('"+idPlayer+"',"+score+")";
-		    	String query2 ="select * savescore";
-		    	rs = st.executeQuery(query);
-		    	System.out.println(rs);	
-		    } catch (Exception e) {
-		      
-		    }   
-		  }
+			con = DriverManager.getConnection(url, user, passwd);
+
+			// Creates an object Statement
+			st  = con.createStatement();
+			// The object dataSend contains the result of the SQL requestL
+
+	    } catch (Exception e) {}
+	}
+	/**
+	 * Method that sends the data to the database with the score and the id of the player
+	 */
+	public void dataSend() {
+	    try {
+	        System.out.println("idPlayer dans l'envoie " + idPlayer);
+	        System.out.println("score dans l'envoie " + score);
+	    	String query ="insert into savescore(id_player,score_player)Values('" + idPlayer + "'," + score + ")";
+	    	String query2 ="select * savescore";
+	    	rs = st.executeQuery(query2);
+	    	System.out.println(rs);
+	    } catch (Exception e) {}
+	}
 }
